1365e436c44a59d2eef6ec2df4307ef242bb41e4
Rizcky Ramadhan <rizckyfwzyr@gmail.com>
2023-03-02 13:03:23 +0700

    Adding Value Objects and domain events to common domain module OK

--- common/common-domain/src/main/java/org/food/ordering/system/domain/entity/AgregateRoot.java
+++
@@ -1,5 +0,0 @@
-package org.food.ordering.system.domain.entity;
-
-public abstract class AgregateRoot<ID> extends BaseEntity<ID> {
-
-}
---
+++ order-service/order-domain/order-domain-core/src/main/java/org/food/ordering/system/order/service/domain/OrderDomainService.java
@@ -0,0 +1,21 @@
+package org.food.ordering.system.order.service.domain;
+
+import org.food.ordering.system.order.service.domain.entity.Order;
+import org.food.ordering.system.order.service.domain.entity.Restaurant;
+import org.food.ordering.system.order.service.domain.event.OrderCancelledEvent;
+import org.food.ordering.system.order.service.domain.event.OrderCreatedEvent;
+import org.food.ordering.system.order.service.domain.event.OrderPaidEvent;
+
+import java.util.List;
+
+public interface OrderDomainService {
+    OrderCreatedEvent validateAndInitiateOrder(Order order, Restaurant restaurant);
+
+    OrderPaidEvent payOrder(Order order);
+
+    void approvedOrder(Order order);
+
+    OrderCancelledEvent cancelOrderPayment(Order order, List<String> failureMessage);
+
+    void cancelOrder(Order order, List<String> failureMessage);
+}
